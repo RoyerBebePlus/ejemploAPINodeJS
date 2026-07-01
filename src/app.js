c45588cab44656e8d4a9b8acb3d8e44e616e65a5
RoyerBebe <tiburon_roge@hotmail.com>
2026-07-01 15:40:35 -0600

    cambiecitos

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ import productRoutes from './routes/product.routes.js';
 
 const app = express();
 
-// Middleware para entender formato JSON
+// MIDDLEWARE PARA ENTENDER JSON
 app.use(express.json());
 
-// Rutas base
+// RUTAS BASE
 app.use('/api/products', productRoutes);
 
-// Ruta por si entran a una URL que no existe
+// RUTA SI ENTRA UNA URL QUE NO EXISTE, CONTESTA
 app.use((req, res) => {
   res.status(404).json({ message: 'Ruta no encontrada' });
 });
